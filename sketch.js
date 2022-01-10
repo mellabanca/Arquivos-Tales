66d37b4e983ba9e9e189fdcd5b617c5168270e22
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-01-10 14:10:45 -0300

    10/01

---
+++ sketch.js
@@ -0,0 +1,75 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var cannonBall;
+
+//exemplos de matrizes
+var matriz1 = [1,2,3];
+console.log(matriz1);
+
+//matrizes com diferentes tipos de dados
+var matriz2 = [5, "Melissa", true];
+console.log(matriz2);
+
+//matriz contendo outras matrizes
+var matriz3 = [matriz1, matriz2];
+console.log(matriz3);
+
+//acessar elementos da matriz
+console.log(matriz1[1]);
+console.log(matriz3[0]);
+console.log(matriz3[1][2]);
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+  angleMode(DEGREES);
+  angle = 15;
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
+  World.add(world, ground);
+
+  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  World.add(world, tower);
+  cannon = new Cannon(180, 110, 130, 100, angle);
+  cannonBall = new CannonBall(cannon.x, cannon.y);
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  Engine.update(engine);
+
+  push();
+  fill("brown");
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  pop();
+
+  push();
+  imageMode(CENTER);
+  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  pop();
+
+  cannon.display();
+  cannonBall.display();
+}
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    cannonBall.shoot();
+  }
+}
